8589d3fd746c0262031685ac2f7ce6153e30e16d
esalaz415 <esalaz415@gmail.com>
2018-10-06 09:52:20 -0700

    initial imports

---
+++ imports.py
@@ -0,0 +1,6 @@
+from math import *
+#These functions must be imported from the math lib within python
+num = 5.3
+print(floor(num))
+print(ceil(num))
+print(sqrt(num))
---
+++ numbers.py
@@ -0,0 +1,10 @@
+num = (-23.32+23.32 + 1 * 5 * (5+2) / 5 + 16.32)
+print(str(num) + " <-- NOW IM A STRING")
+print(10%3)
+
+new_num = -5
+print(abs(new_num))
+print(pow(4, 6))
+print(max(15, 7))
+print(min(15, 7))
+print(round(3.6))
