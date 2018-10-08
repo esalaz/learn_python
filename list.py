8c670a39ad0961ad57e87d1e0a0eab9e3fd98043
esalaz415 <esalaz415@gmail.com>
2018-10-07 19:38:48 -0700

    initial lists

---
+++ list.py
@@ -0,0 +1,20 @@
+friends = ["Jp","Aidan","Brandon","David"]
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends)
+print(friends[-1])
+print(friends[2:4])
+friends[0] = "James"
+print(friends)
+lucky_numbers = [3,2,5,7,8,0,14,17]
+(lucky_numbers.append(32))
+print(lucky_numbers)
+friends.insert(2, "Erik")
+print(friends)
+friends.remove("Erik")
+print(friends)
+friends.pop();
+print(friends)
+friends.clear()
+print(friends)
