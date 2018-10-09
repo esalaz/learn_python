6503b95fe7320cb22d9f16153522b684852c216b
esalaz415 <esalaz415@gmail.com>
2018-10-09 15:57:14 -0700

    initial tuple

---
+++ tuple.py
@@ -0,0 +1,14 @@
+#common use of tuples
+
+coordinates = (4, 5)
+
+#tuples are immutable, cannot be changed or modified
+print(coordinates[0]);
+print(coordinates[1]);
+
+# a tuple is very simple you just create it and you have it.
+# you could even create a list of tuples
+birthdays = [("Erik", 8 , 21 , 1995)],["Maliyah", 3 , 10, 1997];
+
+print(birthdays[0])
+print(birthdays[1])
